f0a08d10ea80b0da3083399f9cd6f1298a4bcddd
tony <trwidick@gmail.com>
2024-03-12 17:50:06 -0700

    Modifications to Cystaldisk screen printout
    Former-commit-id: 3c9314f591b4639f832d25dba46e1c0b39f08400 [formerly 93b52e5e599538a9e72b23210043c6221fb3ab3a] [formerly c41c613455bcb67d8c1d509fcd07505f9b8f0bd0 [formerly 8bf2c2f6b26dcf6bc113558827a527c7c6b09aa8]] [formerly f6d4200b9742bc2682722577d98e70448e419d53 [formerly 77d87047c60834bb6d73a1c868b4cf80adae92ad] [formerly 439b35d29d2856534f2e888e9ed777cd3c384de6 [formerly 5f21563eeeaa78f3c076bb47355f98537ce36bb8]]] [formerly fe68f244a37d13f3165d4533810db42cbe9393d2 [formerly da11467f7f97ed7afab36117d8299b9a1207bdc0] [formerly c467430cd8a2f557cb20b9a0238b1c4027d1ea28 [formerly e7b89b6a18b5f9f2dd32c0e10dc303ea65b9eeff]] [formerly 3b270f1c7fde99000cbfaf2cbc883fae7d9dde18 [formerly 4e9d1f656a03b1e98e3daddec1258b28ae1455fe] [formerly c915e1d022cdd1f12fde14d533b92bd9431aaf58 [formerly 107c37a3fa698b2eb6f70c5f039a2954d2de7f2a]]]]
    Former-commit-id: 18a04aa97c212f801902dba38f3905e20b652f24 [formerly 44162a2a76517db095ba3884d81437e98def069f] [formerly 7670636c3f35e5f04515648ce83153f25c6d768c [formerly bf15aa07f2b65519a6a3d9e6567395b43f352309]] [formerly dccb8084027f47c372729fe20170b14f7844d2b1 [formerly bd479e818e003eed47ec866c8557bb3daf5f9d2b] [formerly 954677d244e6e3a3c1c3f8eb0e9754006df79b72 [formerly b95638431e8c89267dfeed70e8b5ffb6b1f3ccfa]]]
    Former-commit-id: 66c7c40dcb8a7b002a0cea2acacd724227b60e91 [formerly 5f05f030fbee0ebc1a42461c15850abbca4aec32] [formerly b732d8e6387d04bbb7ec5692ef1be1914bc717fa [formerly 62c1af59a9c12c19fec2f0425ca0b9f55f986c4a]]
    Former-commit-id: ffbc64898585cf014c9646e8b63790ca85368ee5 [formerly 14632a39b0bee6c1241d6b56c3d92c0b92fa0310]
    Former-commit-id: e85ddc7e07d6dc33460d432ce8d5a38e6bd91864
    Former-commit-id: 6b4a28f47559f8990c37175735da0839f80217ab

--- test1.ps1
+++ test1.ps1
@@ -14,7 +14,7 @@ $inSmartSection = $false
 # Process each line in the file
 foreach ($line in $smartData) {
     # Ignore empty lines and the line with 'ID RawValues(6) Attribute Name'
-    if ([string]::IsNullOrWhiteSpace($line) -or $line -match "ID RawValues\(6\) Attribute Name") {
+    if ([string]::IsNullOrWhiteSpace($line) -or $line -match "ID Cur Wor Thr RawValues\(6\) Attribute Name") {
         continue
     }
 
@@ -22,14 +22,14 @@ foreach ($line in $smartData) {
         # Start of S.M.A.R.T. section
         $inSmartSection = $true
         Write-Host "-- S.M.A.R.T. --------------------------------------------------------------"
-        Write-Host "ID RawValues(6) Attribute Name"
+        Write-Host "ID Cur Wor Thr RawValues(6) Attribute Name"
         continue
     }
     elseif ($line -match "-- IDENTIFY_DEVICE ------") {
         # End of S.M.A.R.T. section
         $inSmartSection = $false
         Write-Host " "
-        Write-Host "-- IDENTIFY_DEVICE ---------------------------------------------------------"
+        Write-Host "----------------------------------------------------------------------------"
         # Don't break here, as there might be more S.M.A.R.T. sections
         continue
     }
@@ -38,16 +38,15 @@ foreach ($line in $smartData) {
         # Extract the relevant information (ID, RawValues, and Attribute Name)
         $columns = $line -split '\s+'
         $id = $columns[0]
-        $rawValue = $columns[1]
-        $attributeName = $columns[2,3,4,5,6,8,9,10]
+        $rawValue = $columns[4]
+        $attributeName = $columns[5,6,7,8,7,8,9,10]
 
         # Convert the hexadecimal string to a decimal number using BigInteger
         $decimalValue = [System.Numerics.BigInteger]::Parse($rawValue, [System.Globalization.NumberStyles]::HexNumber)
 
         # Display the result
         Write-Host "$id $decimalValue           $attributeName "
-    } else {
-        # If not in a S.M.A.R.T. section, just print the line
+    } elseif (-not ($line -match "^(000:|010:|020:|0AE:|030:|040:|050:|060:|070:|080:|090:|100:|110:|120:|130:|140:|150:|160:|170:|180:|190:|200:|210:|220:|230:|240:|250:|1A0:|0A0:|0B0:|0D0:|1B0:|1C0:|0E0:|0C0:|0F0:|1E0:|1D0:|1F0:|+0 +1 +2 +3 +4 +5 +6 +7 +8 +9 +A +B +C +D +E +F)")) {        # If not in a S.M.A.R.T. section and not a filtered line, print the line
         Write-Host $line
     }
 }
