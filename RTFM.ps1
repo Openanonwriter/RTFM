73eeeb8a4f896f9eeaadfc7d64972d1b42dcd470
Tony <trwidick@gmail.com>
2024-03-13 00:30:05 -0700

    I
    Former-commit-id: 3ae1764e113760c8791d966630ec50bb08589e23 [formerly a83602acb9910316441e4892d232dcef3b4092e9] [formerly 9a3d76af1a1fd53cc1c4871a9ae93500c042b594 [formerly f2571e6294995458be5e067ee416a95a9fe4e3c9]] [formerly e8bd4f5fcc29824fcc1af3d1b6fc807f14afe2ed [formerly 1379d2f23a112c12d7620c403d4d76cbbf43e9d4] [formerly 1a9915bcb9a8d2659e982baf7ae6aec6ad1dd2fc [formerly 9142302286bef8f5ebf5f10604cd340ce0e4b6a1]]] [formerly c170275ef23d100361dc971f1997ee29db6ae581 [formerly d7ec14452a140807c33e9d8427fc271ed45d248f] [formerly 6b21408878bb9f99e2b3d63beae114dceb4eb2cf [formerly 93259c05101c12134248d67f08ad003866b00015]] [formerly bd8c3354bb76acef3451227fb4d2cf06742bfccc [formerly 086dec5cba4a6445999c9d7088e7ebf1d8eed071] [formerly 4325e5220482ab79630da7d41191ba5cec519f0c [formerly 0d41dd328042fd4837cf1209bcd74cbf9f29c764]]]]
    Former-commit-id: a9659dddb40a6607ad77c5cd84c9bc9860637c3d [formerly e0df1f9e39e53f12b7b3dca570bb05ce530af115] [formerly c559225e98bf6c81e979a5cebc176970b5d60f60 [formerly 74203ea435b066f1b0369c4caecf7756d1988eea]] [formerly 9faa8ff38611e877f1f0a520a3d7ab3bdb47e647 [formerly 32815fb5042f15c0eac192d5df9c9443b56ac739] [formerly 2e6128834128967317f6f9b5d5ee3852143ef2dc [formerly 73ba691d29409ffccd5731ebbe0c3b45f9afb1b1]]]
    Former-commit-id: d348bc5a8953bc5e548b757a3a40a91abfc3d028 [formerly 23d6cf9a231038af56e666875dc4558336b87a15] [formerly adfb537c656716480842a802cb250781918f2d31 [formerly 5882a7a8b95b3abd3212799fc972fcf463154b1d]]
    Former-commit-id: 3517b2c8932c4aab1754732e664e9c800a5c8510 [formerly 6eaf161f13ce4b996d00d95a17aa8ce4f6fd1635]
    Former-commit-id: 552158e1094d1cf5b5186290c74b7a23fe7086b5
    Former-commit-id: dafe28be8fb10c11b31978803bc5c38c87dde91a

--- RTFM.ps1
+++ RTFM.ps1
@@ -220,9 +220,114 @@ while ($true) {
 			Write-Host "# of Cores: $($cpuInfo.NumberOfCores)"
 			Write-Host "# of Threads: $($cpuInfo.NumberOfLogicalProcessors)"
 			Write-Host "Max Clock Speed: $($cpuInfo.MaxClockSpeed) Mhz"
-		
-            Write-Host ""
-            Write-Host "----------END----------"
+            
+            #Start of Crystal Disk
+            Start-Sleep -Seconds 5
+            Write-Host "PHYSICAL DISK INFO provided by CrystalDisk" -ForegroundColor Magenta
+
+            #do not need this variable, its already in rtfm later.
+            $currentDirectory = $PSScriptRoot
+
+            $cdiskinfo = Get-ChildItem -Path $currentDirectory -Filter DiskInfo64.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+            & "$cdiskinfo" /CopyExit
+            $diskinfoPath = Get-ChildItem -Path $currentDirectory -Filter DiskInfo.txt -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+            $smartData = Get-Content -Path $diskinfoPath
+            # Read the content of the text file (replace 'x.txt' with the actual file path)
+            # Flag to indicate whether we are within the S.M.A.R.T. section
+            $inSmartSection = $false
+            $unneeded0 = '-- SMART_NVME --------------------------------------------------------------'
+            $unneeded1 = "        0    1    2    3    4    5    6    7    8    9"
+            $unneeded2 = '     \+0 \+1 \+2 \+3 \+4 \+5 \+6 \+7 \+8 \+9 \+A \+B \+C \+D \+E \+F'
+            # Process each line in the file
+            if ($smartData -eq 'j;lasdfja;lsdfja;lsdjfaowerfnalsdfuasodfjas;lerfijas;odlfjasdl;fj'){
+            foreach ($line in $smartData) {
+            # Ignore empty lines and the line with 'ID RawValues(6) Attribute Name'
+            if ([string]::IsNullOrWhiteSpace($line) -or $line -match "ID RawValues\(6\) Attribute Name") {
+                continue
+            }
+
+            if ($line -match "-- S.M.A.R.T. -------") {
+                # Start of S.M.A.R.T. section
+                $inSmartSection = $true
+                Write-Host "-- S.M.A.R.T. --------------------------------------------------------------"
+                Write-Host "ID RawValues(6) Attribute Name"
+                continue
+            }
+            elseif ($line -match "-- IDENTIFY_DEVICE ------") {
+                # End of S.M.A.R.T. section
+                $inSmartSection = $false
+                Write-Host " "
+                Write-Host "----------------------------------------------------------------------------"
+                # Don't break here, as there might be more S.M.A.R.T. sections
+                continue
+            }
+
+            if ($inSmartSection) {
+                # Extract the relevant information (ID, RawValues, and Attribute Name)
+                $columns = $line -split '\s+'
+                $id = $columns[0]
+                $rawValue = $columns[4]
+                $attributeName = $columns[5,6,7,8,7,8,9,10]
+                # Convert the hexadecimal string to a decimal number using BigInteger
+                $decimalValue = [System.Numerics.BigInteger]::Parse($rawValue, [System.Globalization.NumberStyles]::HexNumber)
+
+
+                # Display the result
+                Write-Host "$id $decimalValue           $attributeName "
+                $unneeded0 = "-- SMART_NVME --------------------------------------------------------------"
+                $unneeded1 = "        0    1    2    3    4    5    6    7    8    9"
+                $unneeded2 = '     \+0 \+1 \+2 \+3 \+4 \+5 \+6 \+7 \+8 \+9 \+A \+B \+C \+D \+E \+F'
+                $undeeded3 = '-- SMART_NVME2--------------------------------------------------------------'
+            } elseif (-not ($line -match "^(000:|010:|020:|0AE:|030:|040:|050:|060:|070:|080:|090:|100:|110:|120:|130:|140:|150:|160:|170:|180:|190:|200:|210:|220:|230:|240:|250:|1A0:|0A0:|0B0:|0D0:|1B0:|1C0:|0E0:|0C0:|0F0:|1E0:|1D0:|1F0:|$shittext|$unneeded1|$unneeded2|$undeeded3)")) {        # If not in a S.M.A.R.T. section and not a filtered line, print the line
+                Start-Sleep -Milliseconds 10
+                Write-Host $line
+            }
+        }
+    }
+            else {
+            foreach ($line in $smartData) {
+                # Ignore empty lines and the line with 'ID RawValues(6) Attribute Name'
+                if ([string]::IsNullOrWhiteSpace($line) -or $line -match "ID RawValues\(6\) Attribute Name") {
+                    Start-Sleep 1
+                    continue
+                }
+
+                if ($line -match "-- S.M.A.R.T. -------") {
+                    # Start of S.M.A.R.T. section
+                    $inSmartSection = $true
+                    Write-Host "-- S.M.A.R.T. --------------------------------------------------------------"
+                    Write-Host "ID RawValues(6) Attribute Name"
+                    continue
+                }
+                elseif ($line -match "-- IDENTIFY_DEVICE ------") {
+                    # End of S.M.A.R.T. section
+                    $inSmartSection = $false
+                    Write-Host " "
+                    Write-Host "----------------------------------------------------------------------------"
+                    # Don't break here, as there might be more S.M.A.R.T. sections
+                    continue
+                }
+
+                if ($inSmartSection) {
+                    # Extract the relevant information (ID, RawValues, and Attribute Name)
+                    $columns = $line -split '\s+'
+                    $id = $columns[0]
+                    $rawValue = $columns[1]
+                    $attributeName = $columns[2,3,4,5,6,8,9,10]
+
+                    # Convert the hexadecimal string to a decimal number using BigInteger
+                    $decimalValue = [System.Numerics.BigInteger]::Parse($rawValue, [System.Globalization.NumberStyles]::HexNumber)
+
+
+                    # Display the result
+                    Write-Host "$id $decimalValue           $attributeName "
+                } elseif (-not ($line -match "^($unneeded0|$unneeded1|$unneeded2|000:|010:|020:|0AE:|030:|040:|050:|060:|070:|080:|090:|100:|110:|120:|130:|140:|150:|160:|170:|180:|190:|200:|210:|220:|230:|240:|250:|1A0:|0A0:|0B0:|0D0:|1B0:|1C0:|0E0:|0C0:|0F0:|1E0:|1D0:|1F0:)")) { 
+                    Start-Sleep -Milliseconds 10
+                    Write-Host $line 
+                }
+            }
+        }
+                Write-Host "----------END---------- Press (Q)"
             Read-Host}
       #Network      
 'NDT' { 
--- test1.ps1
+++ test1.ps1
@@ -3,11 +3,8 @@ $currentDirectory = $PSScriptRoot
 
 $cdiskinfo = Get-ChildItem -Path $currentDirectory -Filter DiskInfo64.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
 & "$cdiskinfo" /CopyExit
-#$diskinfoPath = Get-ChildItem -Path $currentDirectory -Filter DiskInfo.txt -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+$diskinfoPath = Get-ChildItem -Path $currentDirectory -Filter DiskInfo.txt -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
 $smartData = Get-Content -Path $diskinfoPath
-# Read the content of the text file (replace 'x.txt' with the actual file path)
-#$smartData = Get-Content -Path  "DiskInfo.txt"
-
 # Read the content of the text file (replace 'x.txt' with the actual file path)
 # Flag to indicate whether we are within the S.M.A.R.T. section
 $inSmartSection = $false
@@ -44,9 +41,13 @@ foreach ($line in $smartData) {
         # Convert the hexadecimal string to a decimal number using BigInteger
         $decimalValue = [System.Numerics.BigInteger]::Parse($rawValue, [System.Globalization.NumberStyles]::HexNumber)
 
-        # Display the result
-        Write-Host "$id $decimalValue           $attributeName "
-    } elseif (-not ($line -match "^(000:|010:|020:|0AE:|030:|040:|050:|060:|070:|080:|090:|100:|110:|120:|130:|140:|150:|160:|170:|180:|190:|200:|210:|220:|230:|240:|250:|1A0:|0A0:|0B0:|0D0:|1B0:|1C0:|0E0:|0C0:|0F0:|1E0:|1D0:|1F0:|+0 +1 +2 +3 +4 +5 +6 +7 +8 +9 +A +B +C +D +E +F)")) {        # If not in a S.M.A.R.T. section and not a filtered line, print the line
-        Write-Host $line
-    }
+     # Display the result
+     Write-Host "$id $decimalValue           $attributeName "
+     $unneeded0 = "-- SMART_NVME --------------------------------------------------------------"
+     $unneeded1 = "        0    1    2    3    4    5    6    7    8    9"
+     $unneeded2 = '     \+0 \+1 \+2 \+3 \+4 \+5 \+6 \+7 \+8 \+9 \+A \+B \+C \+D \+E \+F'
+     $undeeded3 = '-- SMART_NVME2--------------------------------------------------------------'
+ } elseif (-not ($line -match "^(000:|010:|020:|0AE:|030:|040:|050:|060:|070:|080:|090:|100:|110:|120:|130:|140:|150:|160:|170:|180:|190:|200:|210:|220:|230:|240:|250:|1A0:|0A0:|0B0:|0D0:|1B0:|1C0:|0E0:|0C0:|0F0:|1E0:|1D0:|1F0:|$shittext|$unneeded1|$unneeded2|$undeeded3)")) {        # If not in a S.M.A.R.T. section and not a filtered line, print the line
+     Write-Host $line
+ }
 }
